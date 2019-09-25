fc0b31cd3644b1212a8f2b95167db04b8c4941b3
Steven Xia <stevenxia@protonmail.com>
2019-09-24 23:01:27 -0700

    Python type evaluate module.

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,7 +1,7 @@
 import board
 
 
-PIECE_TABLE = [
+_piece_table: list = [
     0, 23, 31,  49,  49, 31, 23,
     0, 31, 43,  61,  61, 43, 31,
     0, 49, 61,  88,  88, 61, 49,
@@ -11,11 +11,13 @@ PIECE_TABLE = [
     0, 23, 31,  49,  49, 31, 23,
 ]
 
-PIECE_TABLE = [(1 << i, PIECE_TABLE[i]) for i in range(49) if i % 7]
+PIECE_TABLE: list = [
+    (1 << i, _piece_table[i]) for i in range(49) if i % 7
+]
 
 
 def evaluate(b: board.Board) -> int:
-    score = 0
+    score: int = 0
     for bit, s in PIECE_TABLE:
         score += (b.yellow_bitboard & bit != 0) * s
         score -= (b.red_bitboard & bit != 0) * s
