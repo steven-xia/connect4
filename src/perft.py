130c0b2dcb560ed367d0d009b2d545649459272d
Steven Xia <stevenxia@protonmail.com>
2019-09-25 16:50:15 -0700

    Fix formatting in perft script.

--- src/perft.py
+++ src/perft.py
@@ -26,9 +26,9 @@ if __name__ == "__main__":
     import time
 
     sys.stdout.write("depth".rjust(6))
-    sys.stdout.write("time (ms)".rjust(11))
+    sys.stdout.write("time (ms)".rjust(12))
     sys.stdout.write("nodes".rjust(12))
-    sys.stdout.write("nps".rjust(10))
+    sys.stdout.write("nps".rjust(11))
     sys.stdout.write("\n")
     sys.stdout.flush()
 
@@ -43,8 +43,8 @@ if __name__ == "__main__":
         speed = 0 if time_taken == 0 else nodes_searched / time_taken
 
         sys.stdout.write(str(depth + 1).rjust(6))
-        sys.stdout.write(str(round(1000 * time_taken)).rjust(11))
+        sys.stdout.write(str(round(1000 * time_taken)).rjust(12))
         sys.stdout.write(str(nodes_searched).rjust(12))
-        sys.stdout.write(str(round(speed)).rjust(10))
+        sys.stdout.write(str(round(speed)).rjust(11))
         sys.stdout.write("\n")
         sys.stdout.flush()
